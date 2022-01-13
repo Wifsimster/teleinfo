6894101497a0d863c41856385232e2b7c9985ebd
Wifsimster <battistella@protonmail.com>
2022-01-13 10:27:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,9 +12,10 @@ raspi.init(() => {
         const serial = new Serial(config.serial)
         serial.open(() => {
 	    let buffer = '', match
-	    let serialNumber, option, subscribedIntensity, index, currentPeriodTariff,
+	    let serialNumber, tariffOption, subscribedIntensity, index, currentPeriodTariff,
 		instantaneousIntensity, instantaneousIntensity01, instantaneousIntensity02, instantaneousIntensity03,
 		maximumIntensity, maximumIntensity01, maximumIntensity02, maximumIntensity03,
+		currentExceededPhase01, currentExceededPhase02, currentExceededPhase03,
 		subscribedPowerExceeded, maximumPower, apparentPower, timeGroup, check
 	    
             serial.on('data', data => {
@@ -42,21 +43,21 @@ raspi.init(() => {
 		    }
 
 		    match = /OPTARIF ([A-Z]+)/g.exec(buffer)    
-		    if(match && match[1] && match[1] !== option) {
-			option = match[1]
-			client.publish(`${topic}/option`, option)
+		    if(match && match[1] && match[1] !== tariffOption) {
+			tariffOption = match[1]
+			client.publish(`${topic}/tariffOption`, tariffOption)
 		    }
 			
 	            match = /ISOUSC ([0-9]+)/g.exec(buffer)
 		    if(match && match[1] && match[1] !== subscribedIntensity) {
 			subscribedIntensity = match[1]
-			client.publish(`${topic}/subscribedIntensity`, subscribedIntensity)
+			client.publish(`${topic}/subscribedIntensity (A)`, subscribedIntensity)
 		    }
 			
 		    match = /BASE ([0-9]+)/g.exec(buffer)
 		    if(match && match[1] && match[1] !== index) {
 			index = match[1]
-			client.publish(`${topic}/index`, index)
+			client.publish(`${topic}/index (Wh)`, index)
 		    }
 			
 		    match = /PTEC ([A-Z]+)/g.exec(buffer)
@@ -68,67 +69,85 @@ raspi.init(() => {
 		    match = /IINST ([0-9]+)/g.exec(buffer)
 		    if(match && match[1] && match[1] !== instantaneousIntensity) {
 			instantaneousIntensity = match[1]
-			client.publish(`${topic}/instantaneousIntensity`, instantaneousIntensity)
+			client.publish(`${topic}/instantaneousIntensity (A)`, instantaneousIntensity)
 		    }
 			
 		    match = /IINST1 ([0-9]+)/g.exec(buffer)
 		    if(match && match[1] && match[1] !== instantaneousIntensity01) {
 			instantaneousIntensity01 = match[1]
-			client.publish(`${topic}/instantaneousIntensity01`, instantaneousIntensity01)
+			client.publish(`${topic}/instantaneousIntensity01 (A)`, instantaneousIntensity01)
 		    }
 			
 		    match = /IINST2 ([0-9]+)/g.exec(buffer)
 		    if(match && match[1] && match[1] !== instantaneousIntensity02) {
 			instantaneousIntensity02 = match[1]
-			client.publish(`${topic}/instantaneousIntensity02`, instantaneousIntensity02)
+			client.publish(`${topic}/instantaneousIntensity02 (A)`, instantaneousIntensity02)
 		    }
 			
 		    match = /IINST3 ([0-9]+)/g.exec(buffer)
 		    if(match && match[1] && match[1] !== instantaneousIntensity03) {
 			instantaneousIntensity03 = match[1]
-			client.publish(`${topic}/instantaneousIntensity03`, instantaneousIntensity03)
+			client.publish(`${topic}/instantaneousIntensity03 (A)`, instantaneousIntensity03)
 		    }
 			
 		    match = /ADPS ([0-9]+)/g.exec(buffer)
 		    if(match && match[1] && match[1] !== subscribedPowerExceeded) {
 			subscribedPowerExceeded = match[1]
-			client.publish(`${topic}/subscribedPowerExceeded`, subscribedPowerExceeded)
+			client.publish(`${topic}/subscribedPowerExceeded (A)`, subscribedPowerExceeded)
+		    }
+			
+		    match = /ADIR1 ([0-9]+)/g.exec(buffer)
+		    if(match && match[1] && match[1] !== currentExceededPhase01) {
+			currentExceededPhase01 = match[1]
+			client.publish(`${topic}/currentExceededPhase01 (A)`, currentExceededPhase01)
+		    }
+			
+		    match = /ADIR2 ([0-9]+)/g.exec(buffer)
+		    if(match && match[1] && match[1] !== currentExceededPhase02) {
+			currentExceededPhase02 = match[1]
+			client.publish(`${topic}/currentExceededPhase02 (A)`, currentExceededPhase02)
+		    }
+			
+		    match = /ADIR3 ([0-9]+)/g.exec(buffer)
+		    if(match && match[1] && match[1] !== currentExceededPhase03) {
+			currentExceededPhase03 = match[1]
+			client.publish(`${topic}/currentExceededPhase03 (A)`, currentExceededPhase03)
 		    }
 			
 		    match = /IMAX ([0-9]+)/g.exec(buffer)
 		    if(match && match[1] && match[1] !== maximumIntensity) {
 			maximumIntensity = match[1]
-			client.publish(`${topic}/maximumIntensity`, maximumIntensity)
+			client.publish(`${topic}/maximumIntensity (A)`, maximumIntensity)
 		    }
 			
 		    match = /IMAX1 ([0-9]+)/g.exec(buffer)
 		    if(match && match[1] && match[1] !== maximumIntensity01) {
 			maximumIntensity01 = match[1]
-			client.publish(`${topic}/maximumIntensity01`, maximumIntensity01)
+			client.publish(`${topic}/maximumIntensity01 (A)`, maximumIntensity01)
 		    }
 			
 		    match = /IMAX2 ([0-9]+)/g.exec(buffer)
 		    if(match && match[1] && match[1] !== maximumIntensity02) {
 			maximumIntensity02 = match[1]
-			client.publish(`${topic}/maximumIntensity02`, maximumIntensity02)
+			client.publish(`${topic}/maximumIntensity02 (A)`, maximumIntensity02)
 		    }
 			
 		    match = /IMAX3 ([0-9]+)/g.exec(buffer)
 		    if(match && match[1] && match[1] !== maximumIntensity03) {
 			maximumIntensity03 = match[1]
-			client.publish(`${topic}/maximumIntensity03`, maximumIntensity03)
+			client.publish(`${topic}/maximumIntensity03 (A)`, maximumIntensity03)
 		    }
 			
 		    match = /PMAX ([0-9]+)/g.exec(buffer)
 		    if(match && match[1] && match[1] !== maximumPower) {
 			maximumPower = match[1]
-			client.publish(`${topic}/maximumPower`, maximumPower)
+			client.publish(`${topic}/maximumPower (W)`, maximumPower)
 		    }
 			
 		    match = /PAPP ([0-9]+)/g.exec(buffer)
 		    if(match && match[1] && match[1] !== apparentPower) {
 			apparentPower = match[1]
-			client.publish(`${topic}/apparentPower`, apparentPower)
+			client.publish(`${topic}/apparentPower (VA)`, apparentPower)
 		    }	
 			
 		    match = /HHPHC ([A-Z]+)/g.exec(buffer)
